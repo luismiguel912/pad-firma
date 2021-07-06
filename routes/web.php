cff2650f373ea45d05409b7fc2fba34baad91b8d
Luis Miguel M.R <miguelluis912@gmail.com>
2021-07-06 12:23:30 -0500

    Se crear las rutas para vista de la firma

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('signaturepad');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// ruta para mostrar la vista de la firma digital
+Route::get( 'signaturepad', [App\Http\Controllers\SignaturePadController :: class , 'index' ]);
+// ruta para guardar la firma digital
+Route::post( 'signaturepad', [App\Http\Controllers\SignaturePadController :: class , 'upload' ])->name( 'signaturepad.upload' );
